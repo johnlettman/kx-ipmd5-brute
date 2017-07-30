9e546136caa6f186bc62baf161a3d48da3d7abf6
John Lettman <johnlettman@openmailbox.org>
2017-07-30 15:18:31 -0400

    Add more comments

--- brute.go
+++ brute.go
@@ -15,11 +15,13 @@ import (
 // Hashes represents a constant-time unique lookup table for checking whether a hash is in the pool of those to break.
 type Hashes map[string]struct{}
 
+// HashResult represents the result of MD5-hashing an IPv4 address.
 type HashResult struct {
 	Hash string
 	IP   net.IP
 }
 
+// incIP returns an IPv4 address one greater than the provided IPv4 address.
 func incIP(ip net.IP) net.IP {
 	i := make(net.IP, 4)
 	copy(i, ip)
@@ -35,6 +37,7 @@ func incIP(ip net.IP) net.IP {
 	return i
 }
 
+// HashIP returns a HashResult pointer with the IP and hex-encoded MD5 value.
 func HashIP(ip net.IP) *HashResult {
 	hasher := md5.New()
 	hasher.Write([]byte(ip.String()))
@@ -48,11 +51,15 @@ func HashIP(ip net.IP) *HashResult {
 	}
 }
 
+// BruteIPNet bruteforces an IPv4 subnet and pushes results matching the hashes sought to the provided channel.
 func BruteIPNet(ipnet *net.IPNet, hashes Hashes, channel chan<- *HashResult) {
+	// iterate over the range of IPv4 addresses in the provided IPv4 subnet,
 	for ip := ipnet.IP; ipnet.Contains(ip); ip = incIP(ip) {
-		result := HashIP(ip)
+		result := HashIP(ip) // hash the current IPv4 address
 
+		// check if the hash exists in the table,
 		if _, ok := hashes[result.Hash]; ok {
+			// output the result,
 			fmt.Printf("match found! %s = %s\n", result.Hash, result.IP.String())
 			channel <- result
 		}
@@ -109,25 +116,29 @@ func init() {
 
 func main() {
 	var (
-		dsn string
+		dsn string // data source name of the MySQL target
 
-		src  string
-		dest string
+		src  string // source file path of the file target
+		dest string // file path of the file destination
 	)
 
-	var ipnet *net.IPNet
 	var hashes Hashes
 
+	// define CLI args,
 	fs := flag.NewFlagSetWithEnvPrefix(os.Args[0], "KX_IPMD5", 0)
 	fs.StringVar(&dsn, "dsn", "", "data source name of the MySQL target")
 	fs.StringVar(&src, "srcfile", "", "source file path of the file target")
 	fs.StringVar(&dest, "destfile", "", "file path of the file destination")
 
+	// parse CLI args,
 	fs.Parse(os.Args[1:])
-	channel := make(chan *HashResult)
-	jobs := make(chan *net.IPNet)
 
-	if len(src) > 0 {
+	// create the channels,
+	channel := make(chan *HashResult) // results channel
+	jobs := make(chan *net.IPNet)     // IPv4 subnet jobs channel
+
+	if len(src) > 0 { // file-based
+		// open the source file,
 		fmt.Printf("reading hashes from file: %s\n", src)
 		file, err := os.Open(src)
 
@@ -135,17 +146,21 @@ func main() {
 			panic(err)
 		}
 
+		// defer closing the source file to when the program is finished,
 		defer func() {
 			fmt.Printf("closing hashes from file: %s\n", src)
 			file.Close()
 		}()
 
+		// read all of the hashes from the source file,
 		hashes = FileHashesRead(file)
-	} else {
+	} else { // unknown!
 		panic(fmt.Errorf("no source method"))
 	}
 
-	if len(dest) > 0 {
+	// ascertain the destination method,
+	if len(dest) > 0 { // file-based
+		// open the destination file,
 		fmt.Printf("writing results to file: %s\n", dest)
 		file, err := os.OpenFile(dest, os.O_WRONLY|os.O_CREATE, 0666)
 
@@ -153,35 +168,44 @@ func main() {
 			panic(err)
 		}
 
+		// defer closing the destination file to when the program is finished,
 		defer func() {
 			fmt.Printf("closing results to file: %s\n", dest)
 			file.Close()
 		}()
 
+		// start a worker for writing the results to the destination file,
 		go FileHashResultWrite(file, channel)
-	} else {
+	} else { // unknown!
 		panic(fmt.Errorf("no destination method"))
 	}
 
+	// count the CPUs
 	numCPUs := runtime.NumCPU()
 
+	// generate a number of workers for all but one processor,
 	for w := 1; w <= numCPUs-1; w++ {
 		go BruteIPNetWorker(w, hashes, jobs, channel)
 	}
 
-	toplev := byte(254)
-	toplevMask := []byte{255, 0, 0, 0}
+	// mask for top-level IPv4 subnets.
+	mask := []byte{255, 0, 0, 0}
 
-	for ; toplev > 0; toplev-- {
-		ipnet = &net.IPNet{
+	// iterate over the range of top-level IPv4 subnets,
+	for toplev := byte(254); toplev > 0; toplev-- {
+		// create the IPv4 network representation of the current top-level
+		ipnet := &net.IPNet{
 			IP:   []byte{toplev, 0, 0, 0},
-			Mask: toplevMask,
+			Mask: mask,
 		}
 
+		// push the job for the current IPv4 subnet,
 		fmt.Printf("add brute forcing job for %v.0.0.0/8\n", toplev)
 		jobs <- ipnet
 	}
 
+	// once all top-level IPv4 subnets have been worked over,
+	// all jobs are done, cleanup,
 	fmt.Println("closing jobs: done")
 	close(jobs)
 }
